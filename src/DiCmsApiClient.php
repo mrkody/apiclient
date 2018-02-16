d79677c41ef0295cf2463f10fc8b01139a5c0985
mrkody <epetrov@difocus.ru>
2018-02-16 20:32:06 +0300

    Making tests and other

--- src/DiCmsApiClient.php
+++ src/DiCmsApiClient.php
@@ -1,44 +1,76 @@
 <?php
 namespace ShopExpress\ApiClient;
 
+use ShopExpress\ApiClient\Response\ApiResponse;
+use ShopExpress\ApiClient\Exception\CurlException;
+use ShopExpress\ApiClient\Exception\CsCartApiException;
+
 class DiCmsApiClient
 {
     const VERSION = '0.1 beta';
     
     const ERROR_API_CALLING = 'You have to specify a method (eg. POST, PUT, ...) and a correct object url to call the API';
-    const ERROR_CURL_ERROR = 'HTTP error while calling the API. Error code and message: ';
-    const ERROR_CSCART_API_MESSAGE = 'Message from API: ';
+    const ERROR_CURL_ERROR = 'HTTP error while calling the API. Error code and message: %s - %s';
+    const ERROR_CSCART_API_MESSAGE = 'Message from API: %s';
+    const ERROR_ARGUMENT_VALUE = 'Parameter `%s` can\'t be empty';
 
-    public static $CURL_OPTS = array(
+    public static $CURL_OPTS = [
         CURLOPT_CONNECTTIMEOUT => 10,
         CURLOPT_RETURNTRANSFER => true,
         CURLOPT_TIMEOUT => 60,
         CURLOPT_USERAGENT => 'dicms-api-php-beta-0.1',
-    );
+    ];
     protected $apiKey;
     protected $userLogin;
     protected $apiUrl;
 
-    public function __construct($config)
+    public function __construct($apiKey, $userLogin, $apiUrl)
     {
-        $this->setUserLogin($config['login']);
-        $this->setApiKey($config['api_key']);
-        $this->setApiUrl($config['api_url']);
+        $this->setApiKey($apiKey);
+        $this->setUserLogin($userLogin);
+        $this->setApiUrl($apiUrl);
     }
 
     public function setApiKey($apiKey)
     {
+        if (empty($apiKey)) {
+            throw new \InvalidArgumentException(
+                sprintf(self::ERROR_ARGUMENT_VALUE, 'apiKey')
+            );
+        } 
         $this->apiKey = $apiKey;
+
+        return $this;
     }
 
     public function setUserLogin($userLogin)
     {
+        if (empty($userLogin)) {
+            throw new \InvalidArgumentException(
+                sprintf(self::ERROR_ARGUMENT_VALUE, 'userLogin')
+            );
+        } 
         $this->userLogin = $userLogin;
+
+        return $this;
     }
 
     public function setApiUrl($apiUrl)
     {
+        if (empty($apiUrl)) {
+            throw new \InvalidArgumentException(
+                sprintf(self::ERROR_ARGUMENT_VALUE, 'apiUrl')
+            );
+        } 
+        if (!filter_var($apiUrl, FILTER_VALIDATE_URL, FILTER_FLAG_SCHEME_REQUIRED | FILTER_FLAG_HOST_REQUIRED)) {
+            throw new \InvalidArgumentException('Parameter `apiUrl` must be valid URL');
+        }
+        if ('/' !== $apiUrl[strlen($apiUrl) - 1]) {
+            $apiUrl .= '/';
+        }
         $this->apiUrl = $apiUrl;
+
+        return $this;
     }
 
     public function getApiKey()
@@ -56,16 +88,7 @@ class DiCmsApiClient
         return $this->apiUrl;
     }
 
-    public function api($method, $objectUrl, $data = '', $params = array())
-    {
-        if (!empty($method) && !empty($objectUrl)) {
-            return $this->makeRequest($objectUrl, $method, $data, $params);
-        } else {
-            throw new Exception(self::ERROR_API_CALLING);
-        }
-    }
-
-    protected function makeRequest($objectUrl, $method, $data = '', $params = array())
+    protected function makeRequest($objectUrl, $method, $data = '', $params = [])
     {
         $ch = curl_init();
 
@@ -73,7 +96,7 @@ class DiCmsApiClient
         //print_r($params);
         $opts[CURLOPT_URL] = $this->initUrl($objectUrl, $params);
         $opts[CURLOPT_USERPWD] = $this->getAuthString();
-        //echo($this->initUrl($objectUrl, $params));
+        //print_r($opts);
         $this->setHeader($opts, 'Content-Type: application/json');
 
         if ($method == 'POST' || $method == 'PUT') {
@@ -103,19 +126,24 @@ class DiCmsApiClient
         }
 
         curl_setopt_array($ch, $opts);
-        $result = curl_exec($ch);
+        $resultBody = curl_exec($ch);
+        $statusCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        $errno = curl_errno($ch);
+        $error = curl_error($ch);
 
-        if ($result === false) {
-            curl_close($ch);
-            throw new Exception(self::ERROR_CURL_ERROR.curl_errno($ch).': '.curl_error($ch));
-        }
         curl_close($ch);
-        return $this->parseResult($result);
+
+        if ($resultBody === false) {
+            throw new CurlException(
+                sprintf(self::ERROR_CURL_ERROR, $errno, $error)
+            );
+        }
+
+        return $this->parseResult($resultBody, $statusCode);
     }
     
-    protected function initUrl($objectUrl, array $params = array())
+    protected function initUrl($objectUrl, array $params = [])
     {
-        //print_r($params);
         $params = !empty($params)? '?' . http_build_query($params) : '';
         return $this->apiUrl . $objectUrl . $params;
     }
@@ -135,42 +163,63 @@ class DiCmsApiClient
         return json_encode($data);
     }
     
-    protected function parseResult($jsonResult)
+    protected function parseResult($resultBody, $statusCode)
     {
-        print_r($jsonResult);
-        $result = (array)json_decode($jsonResult);
-        if (!empty($result['message'])) {
-            throw new Exception(self::ERROR_CSCART_API_MESSAGE.$result['message']);
+        $response = new ApiResponse($statusCode, $resultBody);
+
+        if (!empty($response['message'])) {
+            throw new CsCartApiException(
+                sprintf(self::ERROR_CSCART_API_MESSAGE, $response['message'])
+            );
         } else {
-            return $result;
+            return $response;
+        }
+    }
+
+    public function api($method, $objectUrl, $data = '', $params = [])
+    {
+        if (!empty($method) && !empty($objectUrl)) {
+            return $this->makeRequest($objectUrl, $method, $data, $params);
+        } else {
+            throw new \InvalidArgumentException(self::ERROR_API_CALLING);
         }
     }
     
-    public function get($objectUrl, $params)
+    public function get($objectUrl, $params = [])
     {
-        return $this->makeRequest($objectUrl, 'GET', '', $params);
+        return $this->api('GET', $objectUrl, '', $params);
     }
 
     public function update($objectUrl, $data)
     {
-        return $this->makeRequest($objectUrl, 'PUT', $data);
+        return $this->api('PUT', $objectUrl, $data);
     }
 
     public function create($objectUrl, $data)
     {
-        return $this->makeRequest($objectUrl, 'POST', $data);
+        return $this->api('POST', $objectUrl, $data);
     }
     
     public function delete($objectUrl)
     {
-        return $this->makeRequest($objectUrl, 'DELETE');
+        return $this->api('DELETE', $objectUrl);
     }
     
+    /**
+     * Gets the api version.
+     *
+     * @return string The api version.
+     */
     public function getApiVersion()
     {
         return self::VERSION;
     }
     
+    /**
+     * Gets the cart version.
+     *
+     * @return string The cart version.
+     */
     public function getCartVersion()
     {
         return str_replace("DiCMS: version ", "", strip_tags(file_get_contents($this->apiUrl.'?version')));
--- tests/DiCmsApiClientTest.php
+++ tests/DiCmsApiClientTest.php
@@ -1,19 +1,138 @@
 <?php
 namespace ShopExpress\ApiClient\Test;
 
-use ShopExpress\ApiClient;
+use ShopExpress\ApiClient\DiCmsApiClient;
 
 class DiCmsApiClientTest extends \PHPUnit_Framework_TestCase
 {
-	public function testTrueIsFalse()
+	protected static $config;
+
+	public static function setUpBeforeClass()
+	{
+		static::$config = [
+			'apiKey' => '1234',
+	        'userLogin' => 'admin',
+	        'apiUrl' => 'http://epetrov.kupikupi.org/adm/api/',
+		];
+	}
+
+	public function simpleQuery($instance)
+	{
+		return $instance->get("groups", ['start' => 0, 'limit' => 1]);
+	}
+	
+	/**
+	 * @dataProvider invalidInitConfigProvider
+	 * @expectedException \InvalidArgumentException
+	 */
+	public function testInvalidInit($apiKey, $userLogin, $apiUrl)
     {
-    	$value = (function(){retun false;});
-        $this->assertFalse($value);
+    	$instance = new DiCmsApiClient($apiKey, $userLogin, $apiUrl);
     }
 
-	
-    public function provider()
+    public function testValidInit()
     {
-    	return [[true],[false]];
+    	$instance = new DiCmsApiClient(static::$config['apiKey'], static::$config['userLogin'], static::$config['apiUrl']);
+
+    	return $instance;
+    }
+
+    /**
+     * @depends testValidInit
+     */
+    public function testSendValidInitRequest($instance)
+    {
+    	$response = $this->simpleQuery($instance);
+        $this->assertInstanceOf('ShopExpress\ApiClient\Response\ApiResponse', $response);
+        $this->assertEquals(200, $response->getStatusCode());
+
+        return $instance;
+    }
+
+    /**
+     * @expectedException ShopExpress\ApiClient\Exception\InvalidJsonException
+     */
+    public function testSendInvalidInitRequest()
+    {
+    	$instance = new DiCmsApiClient(static::$config['apiKey'], static::$config['userLogin'], "http://example.ru");
+    	$response = $this->simpleQuery($instance);
+    }
+
+    /**
+     * @expectedException ShopExpress\ApiClient\Exception\InvalidJsonException
+     */
+    public function testSendInvalidInitRequest1()
+    {
+    	$instance = new DiCmsApiClient("wrong", static::$config['userLogin'], static::$config['apiUrl']);
+    	$response = $this->simpleQuery($instance);
+    }
+
+    /**
+     * @expectedException ShopExpress\ApiClient\Exception\InvalidJsonException
+     */
+    public function testSendInvalidInitRequest2()
+    {
+    	$instance = new DiCmsApiClient(static::$config['apiKey'], "wrong", static::$config['apiUrl']);
+    	$response = $this->simpleQuery($instance);
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testSendInvalidInitRequest3()
+    {
+    	$instance = new DiCmsApiClient(static::$config['apiKey'], static::$config['userLogin'], "example.ru");
+    	$response = $this->simpleQuery($instance);
+    }
+
+    /**
+     * @depends testSendValidInitRequest
+     * @expectedException \InvalidArgumentException
+     */
+    public function testSendInvalidObjectUrl($instance)
+    {
+    	$response = $instance->get("");
+    }
+
+    /**
+     * @depends testValidInit
+     */
+    public function testGetApiKey($instance)
+    {
+    	$this->assertEquals(static::$config['apiKey'], $instance->getApiKey());
+    }
+
+    /**
+     * @depends testValidInit
+     */
+    public function testGetUserLogin($instance)
+    {
+    	$this->assertEquals(static::$config['userLogin'], $instance->getUserLogin());
+    }
+
+    /**
+     * @depends testValidInit
+     */
+    public function testGetApiUrl($instance)
+    {
+    	$this->assertEquals(static::$config['apiUrl'], $instance->getApiUrl());
+    }
+
+    public function invalidInitConfigProvider()
+    {
+    	return [
+    		["", "s", "s"],
+    		["s", "", "s"],
+    		["s", "s", ""],
+    		["", "", "s"],
+    		["s", "", ""],
+    		["", "s", ""],
+    		["", "", "example.ru"],
+    	];
+    }
+
+    public static function tearDownAfterClass()
+    {
+    	static::$config = null;
     }
 }
